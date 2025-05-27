1bfb407da83f3251266568d7e26e1f05563a4e5d
Sergio Ardila <sardila535@gmail.com>
2025-05-27 07:14:49 -0500

    Arreglo de imagenes

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,6 +3,8 @@ import "../styles/Benefits.css"
 import Benefits_card from "./Benefits_card"
 import card1 from "../assets/Benefits_star.jpg.webp"
 import card2 from "../assets/Benefits_check.jpg.webp"
+import card3 from "../assets/Benefits_ok.jpg.webp"
+import card4 from "../assets/Benefits_dollar.jpg.webp"
 import division from "../assets/division_benefits.jpg.avif"
 
 
@@ -13,8 +15,8 @@ const Benefits = () => {
         <div className="benefits_card-container">
             <Benefits_card Imgsrc={card1} title="Seasoned Pro at your service" text="With a solid decade in the design biz and big names like Disney, CGV, and Zoos Victoria in the portfolio, you're in for a treat. Expect nothing less than top-drawer, professional designs for your business."/>
             <Benefits_card Imgsrc={card2} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!"/>
-            <Benefits_card Imgsrc={card1} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!"/>
-            <Benefits_card Imgsrc={card1} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!"/>
+            <Benefits_card Imgsrc={card3} title="Speedy Gonzales" text="Who likes to wait, right? That's why most design tasks are zoomed over to you within a 48-hour window. Your projects keep zipping along, no pit stops."/>
+            <Benefits_card Imgsrc={card4} title="Big Bang for your buck" text="Get agency-level design awesomeness at a price that'll make you do a double-take. For a flat monthly fee, you get high-end design work that would cost you an arm and a leg at old school agencies."/>
         </div>
         <div className="benefits_division">
             <img className="benefits_division-img" src={division} alt="" />
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -2,7 +2,9 @@ import React  from "react";
 import "../styles/Jobs.css";
 import Jobs_card from "./Jobs_card";
 import icon1 from "../assets/Work_ilustrate1.jpg.avif"
-import ilustration2 from "../assets/Work_image1.jpg.avif"
+import icon2 from "../assets/Work_ilustrate2.png.avif"
+import ilustration1 from "../assets/Work_image1.jpg.avif"
+import ilustration2 from "../assets/Work_image2.jpg.avif"
 import Separation_banner from "../assets/Separation_banner.jpg.avif"
 
 
@@ -11,8 +13,8 @@ const Jobs = ()=> {
         <>
         <div className="jobs" id="jobs">
             <p className="jobs_title">What we've been up to…</p>
-            <Jobs_card Imgsrc1={icon1} Imgsrc2={ilustration2} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!" />
-             <Jobs_card Imgsrc1={icon1} Imgsrc2={ilustration2} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!" className="jobs_card_second" />
+            <Jobs_card Imgsrc1={icon1} Imgsrc2={ilustration1} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!" />
+             <Jobs_card Imgsrc1={icon2} Imgsrc2={ilustration2} title="Unlimited Do-Overs & Wishlists" text="At a single monthly rate, you get to call the shots with unlimited design requests and revisions. Keep tweaking until your design hits the sweet spot. You dream it, we design it!" className="jobs_card_second" />
         </div>
         <div className="jobs_sepation">
             <img src={Separation_banner} alt=""/>
